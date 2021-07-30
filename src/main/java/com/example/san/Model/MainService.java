bc563eab27a42ec7a41400b6ff4598b82877b351
siyamaksan <57535129+siyamaksan@users.noreply.github.com>
2021-07-30 21:34:14 +0430

    new added

---
+++ src/main/java/com/example/san/Model/MainService.java
@@ -0,0 +1,41 @@
+package com.example.san.Model;
+
+import com.example.san.Model.BaseModel.San_Service;
+import com.example.san.Model.BaseModel.San_User;
+import com.example.san.Model.BaseModel.San_UserService;
+import com.example.san.Model.DAO.IDaoUserService;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MainService extends Thread{
+
+
+    private San_UserService userService;
+    @Autowired
+    private IDaoUserService iDaoUserService;
+
+
+    @Override
+    public void run() {
+        System.out.println("Main Srvice is running!!!!!!!!!!");
+
+        San_Service service = userService.getService();
+        San_User user = userService.getUser();
+
+        long cost = service.getCost();
+
+        user.setCredit(user.getCredit() - cost);
+        userService.setCredit(userService.getCredit() - 1);
+
+        iDaoUserService.Save(userService);
+
+
+    }
+}
